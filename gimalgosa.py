2a606465406ced6bb8d357062022a2cb3850a44d
whwnsgh <skyhot1403@naver.com>
2023-06-09 02:05:16 +0900

    stream

--- gimalgosa.py
+++ gimalgosa.py
@@ -1,3 +1,15 @@
-print("git test")
-print("hello world")
-print("dfdf")
+import streamlit as st
+import control
+import control as ct
+# 전달함수 G1과 G2 정의
+K=1
+G1 = ct.TransferFunction([100],[1])
+G2 = ct.TransferFunction([1],[1,1])
+
+#전체 전달함수
+G3 = ct.feedback(G1 * G2)
+print(G3)
+
+# 극점 출력
+poles = ct.pole(G3)
+print("극점: ", poles)
