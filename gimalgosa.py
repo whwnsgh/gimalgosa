bf2dc74f3669e092d39cc36bf8e62ef560ef9725
whwnsgh <skyhot1403@naver.com>
2023-06-10 01:19:44 +0900

    gg

--- gimalgosa.py
+++ gimalgosa.py
@@ -1,16 +1,4 @@
 import streamlit as st
 import control
 import control as ct
-# 전달함수 G1과 G2 정의
-K=1
-G1 = ct.TransferFunction([100],[1])
-G2 = ct.TransferFunction([1],[1,1])
-
-#전체 전달함수
-G3 = ct.feedback(G1 * G2)
-print(G3)
-
-# 극점 출력
-poles = ct.pole(G3)
-print("극점: ", poles)
-st.hello
+st.title("jhshin")
