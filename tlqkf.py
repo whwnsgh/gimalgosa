2d07eef2eab3112a58f8aa06130bf524218ec172
whwnsgh <skyhot1403@naver.com>
2023-06-10 18:06:19 +0900

    dd

--- tlqkf.py
+++ tlqkf.py
@@ -7,18 +7,15 @@ from scipy import signal
 def main():
     st.title("폐루프 전달함수")
 
-    # 전달함수 G1과 G2 정의
-    G1 = ctl.TransferFunction([100], [1])
-    G2 = ctl.TransferFunction([1], [1, 5, 6])
+   
+    # 전달함수 G(s) = 100/(s^2 + 5s + 6)
+    G = ctl.TransferFunction([100], [1, 5, 6])
 
-    # 폐루프 전달함수 구하기
-    G3 = ctl.feedback(G1 * G2)
+    # 폐루프 전달함수 계산
+    G_closed_loop = ctl.feedback(G)
 
     # 폐루프 전달함수 출력
-    num_str = np.array2string(G3.num[0][0])
-    den_str = np.array2string(G3.den[0][0])
-    G3_str = f"[ {num_str.strip('[]')} ] / [ {den_str.strip('[]')} ]"
-    st.write("폐루프 전달함수:", G3_str)
+    st.write("폐루프 전달함수: ", G_closed_loop)
 
     # unit step 입력에 대한 응답곡선 그리기
     t, y = ctl.step_response(G3)
