25171016a74189215479d84b1ac829742d95b1e0
whwnsgh <skyhot1403@naver.com>
2023-06-10 11:20:08 +0900

    ff

--- final.py
+++ final.py
@@ -1,35 +1,46 @@
+import control
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-import control
 import streamlit as st
+
+def plot_step_response(system):
+    t, y = control.step_response(system)
+    plt.plot(t, y)
+    plt.xlabel('Time')
+    plt.ylabel('Output')
+    plt.title('Step Response')
+    plt.grid()
+
+def plot_bode(system):
+    w, mag, phase = signal.bode(system)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+    ax1.semilogx(w, mag)
+    ax1.set_xlabel('Frequency [rad/s]')
+    ax1.set_ylabel('Magnitude [dB]')
+    ax1.set_title('Bode Plot - Magnitude')
+    ax1.grid()
+    ax2.semilogx(w, phase)
+    ax2.set_xlabel('Frequency [rad/s]')
+    ax2.set_ylabel('Phase [degrees]')
+    ax2.set_title('Bode Plot - Phase')
+    ax2.grid()
+    plt.subplots_adjust(hspace=0.4)
 
 # 전달함수 G(s) 정의
 num = [100]
 den = [1, 5, 6]
-G = signal.TransferFunction(num, den)
+G = control.TransferFunction(num, den)
 
 # 폐루프 전달함수 구하기
 T = control.feedback(G)
 
-# unit step 입력에 대한 응답곡선 그리기
-t, y = control.step_response(T)
-fig1 = plt.figure()
-plt.plot(t, y)
-plt.xlabel('Time')
-plt.ylabel('Output')
-plt.title('Step Response')
-plt.grid()
+# Streamlit 앱 구성
+st.title('Control System Analysis')
+st.header('Step Response')
+plot_step_response(T)
+st.pyplot()
 
-# 주파수 응답을 보드선도로 그리기
-w, mag, phase = signal.bode(G)
-fig2 = plt.figure()
-plt.semilogx(w, mag)
-plt.xlabel('Frequency [rad/s]')
-plt.ylabel('Magnitude [dB]')
-plt.title('Bode Plot')
-plt.grid()
-
-# 그래프 출력
-st.pyplot(fig1)
-st.pyplot(fig2)
+st.header('Bode Plot')
+plot_bode(G)
+st.pyplot()
