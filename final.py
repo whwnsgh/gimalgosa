cf53ceb3a4d55126e3da6bde47d9fe0abb0ea27a
whwnsgh <skyhot1403@naver.com>
2023-06-10 19:13:33 +0900

    dsf

--- final.py
+++ final.py
@@ -8,20 +8,21 @@ def main():
     st.title("제어공학 기말고사")
     st.write("202021089 조준호")
 
-     # 전달함수 G(s) = 100/((s+2)(s+3))
-    G = ctl.TransferFunction([100], [1, 5, 6])
+    # 전달함수 G1과 G2 정의
+    G1 = ctl.TransferFunction([100], [1])
+    G2 = ctl.TransferFunction([1], [1, 5, 6])
 
     # 폐루프 전달함수 구하기
-    G_feedback = ctl.feedback(G)
+    G3 = ctl.feedback(G1 * G2)
 
     # 폐루프 전달함수 출력
-    num_str = ctl.pprint(G_feedback.num[0][0])
-    den_str = ctl.pprint(G_feedback.den[0][0])
-    G_str = f"[ {num_str} ] / [ {den_str} ]"
-    st.write("폐루프 전달함수:", G_str)
+    num_str = np.array2string(G3.num[0][0])
+    den_str = np.array2string(G3.den[0][0])
+    G3_str = f"[ {num_str.strip('[]')} ] / [ {den_str.strip('[]')} ]"
+    st.write("폐루프 전달함수:", G3_str)
 
     # unit step 입력에 대한 응답곡선 그리기
-    t, y = ctl.step_response(G)
+    t, y = ctl.step_response(G3)
     plt.figure()
     plt.plot(t, y)
     plt.xlabel("Time")
