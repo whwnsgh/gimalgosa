c5f7a4082fdfc64e9ca7c583d900037f7dea08e0
whwnsgh <skyhot1403@naver.com>
2023-06-10 13:31:56 +0900

    dd

--- final.py
+++ final.py
@@ -1,7 +1,6 @@
 import control
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
 import streamlit as st
 
 def plot_step_response(system):
@@ -13,14 +12,14 @@ def plot_step_response(system):
     plt.grid()
 
 def plot_bode(system):
-    w, mag, phase = signal.bode(system)
+    mag, phase, omega = control.bode(system, Plot=False)
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
-    ax1.semilogx(w, mag)
+    ax1.semilogx(omega, mag)
     ax1.set_xlabel('Frequency [rad/s]')
     ax1.set_ylabel('Magnitude [dB]')
     ax1.set_title('Bode Plot - Magnitude')
     ax1.grid()
-    ax2.semilogx(w, phase)
+    ax2.semilogx(omega, phase)
     ax2.set_xlabel('Frequency [rad/s]')
     ax2.set_ylabel('Phase [degrees]')
     ax2.set_title('Bode Plot - Phase')
@@ -37,10 +36,19 @@ T = control.feedback(G)
 
 # Streamlit 앱 구성
 st.title('Control System Analysis')
-st.header('Step Response')
-plot_step_response(T)
-st.pyplot()
 
+# 전달함수 출력
+st.header('Transfer Function')
+st.latex(r'G(s) = \frac{100}{{(s+2)(s+3)}}')
+
+# 단위계단입력의 응답곡선 출력
+st.header('Step Response')
+fig_step = plt.figure()
+plot_step_response(T)
+st.pyplot(fig_step)
+
+# 주파수응답 출력
 st.header('Bode Plot')
+fig_bode = plt.figure()
 plot_bode(G)
-st.pyplot()
+st.pyplot(fig_bode)
