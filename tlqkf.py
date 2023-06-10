e5752d94324cab3ac202a78098acead103526e39
whwnsgh <skyhot1403@naver.com>
2023-06-10 16:08:48 +0900

    fddfdsf

---
+++ tlqkf.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import control as ctl
+import streamlit as st
+
+st.title("202021089 조준호")
+
+# 전달함수 G1과 G2 정의
+G1 = ctl.TransferFunction([100], [1])
+G2 = ctl.TransferFunction([1], [1, 5, 6])
+
+# 전달함수 G = G1 * G2 전방향 경로 / 직렬 결합
+G = G1 * G2
+
+# G3 = feedback(G, sign=-1, name=None) 함수를 사용하여 피드백 블록 계산
+G3 = ctl.feedback(G, sign=-1)
+
+# 전달함수의 분자와 분모
+num = G3.num[0][0]
+den = G3.den[0][0]
+
+# 전달함수 출력
+st.write("전달함수: ", num, "/", den)
