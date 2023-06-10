26cf6b986ad95bef754dd343bbadc44a65ecd586
whwnsgh <skyhot1403@naver.com>
2023-06-10 15:27:50 +0900

    d

--- final.py
+++ final.py
@@ -1,42 +1,36 @@
-import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
 import control
+import streamlit as st
 
 def main():
     st.title("폐루프 전달함수 시뮬레이션")
 
-    # 전달함수 G1과 G2 정의
-    K = 1
-    G1 = control.TransferFunction([100], [1])
-    G2 = control.TransferFunction([1], [1, 5, 6])
+    # 전달함수 G(s) 정의
+    G = control.TransferFunction([100], [1, 5, 106])
 
-    # 전체 전달함수
-    G3 = control.feedback(G1 * G2)
+    # 폐루프 전달함수 계산
+    G_closed_loop = control.feedback(G, 1)
 
-    # 전체 전달함수의 분자와 분모
-    num = G3.num
-    den = G3.den
+    # Unit step 입력 생성
+    t = np.linspace(0, 10, 1000)
+    u = np.heaviside(t, 1)
 
-    # 극점과 영점 찾기
-    zeros, poles, _ = signal.tf2zpk(num, den)
+    # 폐루프 응답 계산
+    t, y = control.step_response(G_closed_loop, T=t)
 
     # 폐루프 전달함수 출력
     st.title("폐루프 전달함수")
-    st.latex(f"G(s) = \\frac{{{num}}}{{{den}}}")
+    st.latex("G(s) = \\frac{100}{{s^2 + 5s + 106}}")
 
-    # 극점과 영점 그래프 그리기
+    # Unit step 입력의 응답곡선 그리기
     fig, ax = plt.subplots()
-    ax.scatter(np.real(poles), np.imag(poles), marker='x', color='red', label='Poles')
-    ax.scatter(np.real(zeros), np.imag(zeros), marker='o', color='blue', label='Zeros')
-    ax.set_xlabel('Real Axis')
-    ax.set_ylabel('Imaginary Axis')
-    ax.set_title('Poles and Zeros')
+    ax.plot(t, u, label="Unit Step Input")
+    ax.plot(t, y, label="System Response")
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Amplitude')
+    ax.set_title('Step Response')
     ax.legend()
-    ax.grid()
-
-    # 그래프 출력
     st.pyplot(fig)
 
 if __name__ == '__main__':
