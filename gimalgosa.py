98b98de8fdfe72e3efc8c2194446377f3dcbad3d
whwnsgh <skyhot1403@naver.com>
2023-06-10 16:05:05 +0900

    ss

--- gimalgosa.py
+++ gimalgosa.py
@@ -15,7 +15,9 @@ G = G1 * G2
 # G3 = feedback(G, sign=-1, name=None) 함수를 사용하여 피드백 블록 계산
 G3 = ctl.feedback(G, sign=-1)
 
+# 전달함수의 분자와 분모
+num = G3.num[0][0]
+den = G3.den[0][0]
+
 # 전달함수 출력
-num, den = ctl.tf2ss(G3.num, G3.den)
-G3_str = ctl.ss2str(num, den)
-st.write("전달함수:", G3_str)
+st.write("전달함수: ", num, "/", den)
