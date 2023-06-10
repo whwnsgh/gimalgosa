e6b353152db8c884aae7c49a4222d96c49c7edfb
whwnsgh <skyhot1403@naver.com>
2023-06-10 16:14:37 +0900

    dfdf

--- tlqkf.py
+++ tlqkf.py
@@ -15,10 +15,12 @@ G = G1 * G2
 # G3 = feedback(G, sign=-1, name=None) 함수를 사용하여 피드백 블록 계산
 G3 = ctl.feedback(G, sign=-1)
 
-# 전달함수의 분자와 분모
-num = G3.num[0][0]
-den = G3.den[0][0]
+# 전달함수의 분자와 분모 계수 추출
+num_coef = G3.num[0][0].tolist()
+den_coef = G3.den[0][0].tolist()
 
 # 전달함수 출력
-G3_str = f"{num}/{den}"
+num_str = " ".join(str(c) for c in num_coef)
+den_str = " ".join(str(c) for c in den_coef)
+G3_str = f"[ {num_str} ] / [ {den_str} ]"
 st.write("전달함수:", G3_str)
